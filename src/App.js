14552c2e04b12720f335f15df3a48454a47a340b
Eldridge Gomes <gomes.eldridge@gmail.com>
2020-11-05 22:15:01 +0530

    Removed redundancy

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Main from './box/Main';
 
 function App() {
+  //anything added here is as good as adding in the body of an HTML page
   return (
     <>
       <Main />
--- src/box/Main.js
+++ src/box/Main.js
@@ -4,23 +4,17 @@ import './Main.css';
 
 function Main (props) {
     return (
-        <html lang='en'>
-            <head>
-                <meta charset='UTF-8'/>
-                <title>Boxes Are Easy!</title>
-            </head>
-            <body>
-                <h1>Headings Are Block Elements</h1>
+        <>
+            <h1>Headings Are Block Elements</h1>
 
-                <p>Paragraphs are blocks, too. <em>However</em>, &lt;em&gt; and &lt;strong&gt;
-                elements are not. They are <strong>inline</strong> elements.</p>
-                
-                <p>Block elements define the flow of the HTML document, while inline elements
-                do not.</p>
+            <p>Paragraphs are blocks, too. <em>However</em>, &lt;em&gt; and &lt;strong&gt;
+            elements are not. They are <strong>inline</strong> elements.</p>
+            
+            <p>Block elements define the flow of the HTML document, while inline elements
+            do not.</p>
 
-                <div className="button">Button</div>
-            </body>
-        </html>
+            <div className="button">Button</div>
+        </>
     );
 }
 
