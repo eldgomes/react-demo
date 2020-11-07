faaf51a4fbbd60ddaf1ebbaa51e8b159b74cda7e
Eldridge Gomes <gomes.eldridge@gmail.com>
2020-11-07 09:53:40 +0530

    Responsive images and optimization

--- src/responsive/Page.js
+++ src/responsive/Page.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-import header from '../images/responsive/header.svg';
-import content from '../images/responsive/content.svg';
+//import header from '../images/responsive/header.svg';
+//import content from '../images/responsive/content.svg';
+import illustrationSmall from '../images/responsive/illustration-small.png';
+import illustrationBig from '../images/responsive/illustration-big.png';
+import photoSmall from '../images/responsive/photo-small.jpg';
+import photoBig from '../images/responsive/photo-big.jpg';
 import signUp from '../images/responsive/sign-up.svg';
 import feature from '../images/responsive/feature.svg';
 
@@ -13,10 +17,19 @@ function Page (props) {
             <div className='page'>
                 <div className='section menu'></div>
                 <div className='section header'>
-                    <img src={header}/>
+                    <div class='photo'>
+                        <img src='images/photo-small.jpg'
+                            srcSet={`${photoBig} 2000w, ${photoSmall} 1000w`}
+                            sizes='(min-width: 960px) 960px,
+                                    100vw'/>
+                    </div>
                 </div>
                 <div className='section content'>
-                    <img src={content}/>
+                <div class='illustration'>
+                    <img src={illustrationSmall}
+                        srcSet={`${illustrationSmall} 1x, ${illustrationBig} 2x`}
+                        style={{maxWidth: '500px'}}/>
+                </div>
                 </div>
                 <div className='section sign-up'>
                     <img src={signUp}/>
