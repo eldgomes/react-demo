844202735bb7db214136c9dff4fcee35722c6855
Eldridge Gomes <gomes.eldridge@gmail.com>
2020-11-08 20:01:49 +0530

    Hazira initial commit

---
+++ src/hazira/Responsive.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Page from './Page';
+
+import './Responsive.css';;
+
+function Responsive (props) {
+    return (
+        <>
+            <Page />
+        </>
+    );
+}
+
+export default Responsive;
